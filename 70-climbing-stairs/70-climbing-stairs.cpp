0652d534663956fd3f69362ff4aa3d6b9a22a1aa
Anchal <80024182+anchaljs@users.noreply.github.com>
2022-07-06 16:58:32 +0530

    Time: 3 ms (35.41%), Space: 6 MB (56.37%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     
     int climbStairs(int n) {
-        int dp[n+1];
+      int dp[n+1];
         dp[0]=1;
-        for(int i=1;i<=n;i++){
-        if(i==1){
-            dp[i]=dp[i-1];
-        }
-        else {
-        
-            dp[i]=dp[i-1]+dp[i-2];
-        }
+        for(int i=1;i<n+1;i++){
+            if(i<2){
+                dp[i]=dp[i-1];
+            }
+            else{
+                dp[i]=dp[i-1]+dp[i-2];
+            }
         }
         return dp[n];
         
